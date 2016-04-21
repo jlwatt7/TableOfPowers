b594ed79371ce7c5412e06044d3345e0d3a8b2d2
Jamilah <jlwatt7@gmail.com>
2016-04-21 09:25:22 -0400

    Pushing Project

---
+++ src/tableOfPower.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+
+public class tableOfPower {
+	public static void main(String[] args) {
+		// Welcome the user
+        System.out.println("Welcome to the Squares and Cubes table");
+        System.out.println();
+
+        Scanner sc = new Scanner(System.in);
+        String choice = "y";
+
+        do
+        {
+            // Get input from the user
+            System.out.print("Enter an integer: ");
+            int integer = sc.nextInt();
+
+            // Create a header
+            String header = "Number  " + "Squared " + "Cubed   " + "\n"
+                        +   "======  " + "======= " + "=====   ";
+            System.out.println(header);
+
+            int square = 0;
+            int cube = 0;
+
+            String row = "";
+            for (int i = 1; i <= integer; i++)
+            {
+
+                square = i * i;
+                cube = i * i * i;
+
+                row = i + "       " + square + "       " + cube;
+                System.out.println(row);
+            }
+            // See if the user wants to continue
+            System.out.print("Continue? (y/n): ");
+            choice = sc.next();
+            System.out.println();
+
+        }
+        while (!choice.equalsIgnoreCase("n"));  
+    }
+
+}
